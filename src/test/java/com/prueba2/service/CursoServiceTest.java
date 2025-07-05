37874f4de5822b9fb5cdb1e41679b9544d86fd14
oscar-abud <oscarpalma.vpr2@gmail.com>
2025-07-05 18:15:05 -0400

    Curso Service test completado con exito

--- src/test/java/com/prueba2/service/CursoServiceTest.java
+++ src/test/java/com/prueba2/service/CursoServiceTest.java
@@ -1,6 +1,7 @@
 package com.prueba2.service;
 
 import java.util.Date;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -8,17 +9,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.prueba2.model.Curso;
 import com.prueba2.model.Rol;
 import com.prueba2.model.Usuario;
 import com.prueba2.repository.CursoRepository;
-import com.prueba2.repository.UsuarioRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class CursoServiceTest {
@@ -31,19 +31,19 @@ public class CursoServiceTest {
     @InjectMocks
     private CursoService cursoService;
 
-    @Autowired
-    UsuarioRepository usuarioRepository;
 
-
-    @Test
-    void testGuardarCurso(){
-
-        //Crear Rol (si tu modelo tiene nombre, id, etc.)
+    //Crear Rol
+    private Rol getRol(){
         Rol rol = new Rol();
         rol.setId_rol(1L);
         rol.setNombre_rol("ADMIN");
+        return rol;
+    }
 
-        //Usuario
+    //Creando Usuario
+    private Usuario getUser(){
+        Rol rol = getRol();
+
         Usuario usuario = new Usuario();
         usuario.setRut("12345678-9");
         usuario.setNombre("Juan");
@@ -52,6 +52,14 @@ public class CursoServiceTest {
         usuario.setContrasena("1234segura");
         usuario.setfechaRegistro(new Date());
         usuario.setRol(rol);
+        return usuario;
+    }
+        
+
+    @Test
+    void testGuardarCurso(){
+        //Usuario
+        Usuario usuario = getUser();
 
         Curso curso = new Curso(106L, "Python", "Aprendiendo Python", "TERMINADO", usuario);
         when(cursoRepository.save(curso)).thenReturn(curso);
@@ -63,4 +71,32 @@ public class CursoServiceTest {
         verify(cursoRepository, times(1)).save(curso);
     }
 
+    @Test
+    void buscarCurso(){
+
+        //Usuario
+        Usuario usuario = getUser();
+        
+        //Curso
+        Curso curso = new Curso(106L, "Python", "Aprendiendo Python", "TERMINADO", usuario);
+
+        when(cursoRepository.findById(106L)).thenReturn(Optional.of(curso));
+
+        Optional <Curso> resultadoCurso = cursoService.obtenerPorId(106L);
+
+        assertNotNull(resultadoCurso);
+        assertEquals("Python", resultadoCurso.get().getNombre_curso());
+        verify(cursoRepository, times(1)).findById(106L);
+
+    }
+
+    @Test
+    void eliminarCurso(){
+        Long idCurso = 106L;
+        doNothing().when(cursoRepository).deleteById(idCurso);
+
+        cursoService.eliminar(idCurso);
+        
+        verify(cursoRepository, times(1)).deleteById(idCurso);
+    }
 }
